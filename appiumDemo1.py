d53b6f2a9339cbd09b2449a3ab44cf2bc6371700
Tser <807447312@qq.com>
2015-10-19 16:35:21 +0800

    getPageUrl

---
+++ appiumDemo1.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from appium import webdriver
+from time import sleep
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '4.4'
+desired_caps['deviceName'] = ''
+desired_caps['appPackage'] = ''
+desired_caps['appActivity'] = ''
+# desired_caps['app-wait-activity'] = ''
+desired_caps['unicodeKeyboard'] = True
+desired_caps['resetKeyboard'] = True
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+sleep(0.5)
+driver.find_element_by_name(u"全部").click()
+sleep(1)
+driver.find_element_by_id('com.zline.consumer:id/iv_common_header_right').click()
+sleep(1)
+word = driver.find_element_by_id('com.zline.consumer:id/et_search')
+word.clear()
+word.send_keys(u'中文')
