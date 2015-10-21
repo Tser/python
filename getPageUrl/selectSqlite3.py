ad9325568580524be9462f729258ecf0d1c5b554
tser <807447312@qq.com>
2015-10-21 20:39:46 +0800

    update

--- getPageUrl/selectSqlite3.py
+++ getPageUrl/selectSqlite3.py
@@ -5,7 +5,7 @@ import sqlite3
 
 sql_ser = sqlite3.connect('a_href.db')
 sql_cu = sql_ser.cursor()
-sql_cu.execute('select count(*) from dianping where url="http://e.dianping.com/shopclaim/introduce/introducelogin"')
+sql_cu.execute('select count(*) from baidu')
 # sql_cu.execute('delete from dianping ')
 # sql_ser.commit()
 # sql_cu.execute('insert into dianping(netloc, url, status) values("www.dianping.com", "http://www.dianping.com/login", "OK")')
