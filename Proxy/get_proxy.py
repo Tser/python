83b8596f522c2c74403957a6637c0c7ea0de2ba8
tser <807447312@qq.com>
2015-10-22 00:18:12 +0800

    update

--- Proxy/get_proxy.py
+++ Proxy/get_proxy.py
@@ -2,10 +2,9 @@
 # coding=utf-8
 
 from ghost import Ghost
-import stackless
+# import stackless
 
 ghost = Ghost()
 
 with ghost.start() as session:
-    page, = session.open('http://www.oschina.net/project/tag/216/ostools?lang=0&os=0&sort=view&p=1')
-    assert page.http_status == 200 and 'bootin' in page.content
+    page, resources = session.open("http://jeanphix.me")
