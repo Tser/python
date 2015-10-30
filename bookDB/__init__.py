1a2aff1e1332ed7041c11e47e1d76428a8ff88de
tser <807447312@qq.com>
2015-10-30 21:39:43 +0800

    update

---
+++ bookDB/__init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+
+'''
+    1、当当网                   http://book.dangdang.com/
+    2、京东商城                  http://book.jd.com/
+    3、1号店                   http://www.yhd.com/?tracker_u=2225501
+    4、亚马逊                   http://www.amazon.cn/b/?ie=UTF8&node=658390051&tag=baiduiclickcn-23&ref=pz_ic_db_j_9ops9
+    5、苏宁易购                  http://book.suning.com/
+
+    通过最畅销的书，来判断出当下最热的行业或者走向
+'''
---
+++ getPageUrl/urlDemo.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+'''
+    1、将url进行MD5加密
+    2、保存MD5值
+    3、再次抓取的url与已存的MD5进行对比
+'''
