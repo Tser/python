68aaa715612a04964735c6662d31a7bfb1dffc1f
tser <807447312@qq.com>
2015-11-05 10:09:56 +0800

    update

--- getPageUrl/selectSqlite3.py
+++ getPageUrl/selectSqlite3.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python
 #coding=utf-8
 
-import sqlite3, re
+import sqlite3
 
-sql_ser = sqlite3.connect('a_href.db')
-sql_cu = sql_ser.cursor()
-sql_cu.execute('select * from baidu')
-# # sql_cu.execute('delete from baidu ')
-sql_ser.commit()
-print sql_cu.fetchall()
-sql_cu.close()
-sql_ser.close()
+try:
+    sql_ser = sqlite3.connect('a_href.db')
+    sql_cu = sql_ser.cursor()
+    sql_cu.execute('select * from baidu')
+    # # sql_cu.execute('delete from baidu ')
+    sql_ser.commit()
+    print sql_cu.fetchall()
+except Exception, e:
+    print e.message
+finally:
+    sql_cu.close()
+    sql_ser.close()
---
+++ getPageUrl/sqlDemo.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# coding=utf-8
+import sqlite3
+
+sql = sqlite3.connect('a_href.db')
+cur = sql.cursor()
+cur.executescript('sql3.sql')
+sql.commit()
+print cur.fetchone()
+cur.close()
+sql.close()
