1a8da8fb9b245ae3918fe03204de9881189aa699
tser <807447312@qq.com>
2015-11-06 13:16:08 +0800

    update

--- appiumDemo/android_call_to_U.py
+++ appiumDemo/android_call_to_U.py
@@ -18,7 +18,9 @@ caps['unicodeKeyboard'] = True
 caps['resetKeyboard'] = True
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', caps)
+# 元素属性text值唯一时，可以直接使用name来定位元素
 driver.find_element_by_name(u'拨号').click()
+#元素text值不唯一时，可以通过className的值来定位（定位好className，判断属性值是否为某一特定的值）
 call = driver.find_element_by_class_name('android.widget.Button')
 assert call.get_attribute('text') == u'拨号'
 call.click()
