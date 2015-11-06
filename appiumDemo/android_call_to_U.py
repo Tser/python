9d45629c951fe3eaafb3c26767d152b2ce14aff5
tser <807447312@qq.com>
2015-11-06 13:14:20 +0800

    update

--- appiumDemo/android_call_to_U.py
+++ appiumDemo/android_call_to_U.py
@@ -2,16 +2,23 @@
 # coding=utf-8
 
 '''
+        platformName: MI NOTE
         Test android call...
+        version=0.1
 '''
 
 from appium import webdriver
-
 caps = {}
-caps['platfromName'] = 'Android'
-caps['platfromVersion'] = '4.4'
+caps['platformName'] = 'Android'
+caps['platformVersion'] = '4.4'
 caps['deviceName'] = ''
-caps['appPackage'] = ''
-caps['appActivity'] = ''
+caps['appPackage'] = 'com.android.contacts'
+caps['appActivity'] = '.activities.PeopleActivity'
 caps['unicodeKeyboard'] = True
 caps['resetKeyboard'] = True
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', caps)
+driver.find_element_by_name(u'拨号').click()
+call = driver.find_element_by_class_name('android.widget.Button')
+assert call.get_attribute('text') == u'拨号'
+call.click()
